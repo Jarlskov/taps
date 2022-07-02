8a1c9d8325a896a189b331c092583d60694e1b23
Jesper Jarlskov <jesper@jarlskov.dk>
2022-07-02 23:03:15 +0200

    Remove scraper class name from Bar model

--- app/Models/Bar.php
+++ app/Models/Bar.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Scrapers\ScraperInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
@@ -31,7 +32,15 @@ class Bar extends Model
 
     public function updateTaplist(): void
     {
-        $scraper = new $this->class($this);
+        $scraper = $this->getScraper();
         $scraper->scrape();
+    }
+
+    private function getScraper(): ScraperInterface
+    {
+        $name = preg_replace('/\s+/', '', $this->name);
+        $classname = '\\App\\Scrapers\\' . $name . 'Scraper';
+
+        return new $classname($this);
     }
 }
